ed9fa554832ba42c68ff7cf7ff0374d2527c1b03
Taka Desktop <tyabe@purdue.edu>
2021-05-04 12:59:18 -0400

    commit

--- src/motifs/getmotifs.java
+++ src/motifs/getmotifs.java
@@ -4,10 +4,19 @@ public class getmotifs {
 
 	/**
 	 * 1. get home locations from pre-disaster data for each individual
-	 * 2. get post-disaster nighttime staypoints for each day 
+	 * 2. get post-disaster night time stay points for each day 
 	 * 3. compute motifs for each ID
 	 * 
 	 */
+	
+	
+	public static void main(String[] args) {
+		
+		// 1. 
+		System.out.println("aa");
+		
+		
+	}
 	
 	
 	
