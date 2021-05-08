0e99aaac36ad935bf41a6b38d19359bd10a4996e
Taka Yabe <yabe0505@iis.u-tokyo.ac.jp>
2021-05-08 19:04:18 -0400

    commit

--- src/motifs/displacement.java
+++ src/motifs/displacement.java
@@ -2,9 +2,23 @@ package motifs;
 
 public class displacement {
 
-	public static void homeestimation() {
+	/**
+	 * - get logs per user per day (night time)
+	 * - estimate where user was staying the night the longest between 10pm-4am
+	 * - compute for each user per day 
+	 */
+	
+	public static void intomap(String[] args) {
+		
+		String root = "D:/WB_Mexico/";
+		String gpsdataroot = root+"gpsdata_eq/";
+		String outpath = root+"results_eq/";
+		
+		
+		
 		
 	}
+	
 	
 	
 }
