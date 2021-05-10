030267fa11254655554a849667686ffb09092430
Taka Yabe <tyabe@purdue.edu>
2021-05-10 00:17:21 -0400

    motifs

--- src/motifs/getmotifs.java
+++ src/motifs/getmotifs.java
@@ -1,8 +1,10 @@
 package motifs;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.TreeMap;
@@ -10,6 +12,61 @@ import java.util.TreeMap;
 import jp.ac.ut.csis.pflow.geom.LonLat;
 
 public class getmotifs {
+
+	public static void computemotifs(
+			File in, 
+			Double thres, 
+			File out) throws IOException {
+		HashMap<String, TreeMap<String, LonLat>> map = intomap(in);
+		for(String id : map.keySet()) {
+			TreeMap<String, LonLat> ps = map.get(id);
+			getsequence(id, ps, thres, out);
+		}
+	}
+
+	public static void getsequence(
+			String id,
+			TreeMap<String, LonLat> date_ll,
+			Double thres,
+			File out
+			) throws IOException {
+		// temp map for lonlat and node number
+		HashMap<LonLat, Integer> ll_num = new HashMap<LonLat, Integer>();
+		String result = "";
+		Integer clusnum = 1;
+		for(String date : date_ll.keySet()) {
+			LonLat p = date_ll.get(date);
+			// first point 
+			if(ll_num.isEmpty()) {
+				ll_num.put(p, clusnum);
+				result = result + date+","+clusnum+";";
+				clusnum+=1;
+			}
+			// from second point
+			else {
+				// check in all clusters
+				String flag = "no";
+				for(LonLat clus : ll_num.keySet()) {
+					if(p.distance(clus)<thres) {
+						result = result + date+","+clusnum+";";
+						flag = "yes";
+						break;
+					}
+				}
+				// if point is not near any cluster center
+				if(flag.equals("no")) {
+					ll_num.put(p, clusnum);
+					result = result + date+","+clusnum+";";
+					clusnum+=1;
+				}
+			}
+		}
+		BufferedWriter bw = new BufferedWriter(new FileWriter(out, true));
+		bw.write(id+"\t"+result);
+		bw.newLine();
+		bw.close();
+	}
+
 
 	public static HashMap<String, TreeMap<String, LonLat>> intomap(File in) throws IOException{
 		HashMap<String, TreeMap<String, LonLat>> map = new HashMap<String, TreeMap<String, LonLat>>();
@@ -32,48 +89,5 @@ public class getmotifs {
 		br.close();
 		return map;
 	}
-	
-	
-	public static HashMap<String, Integer> getsequence(
-			HashMap<String, LonLat> date_ll,
-			Double thres
-			) {
-		// temp map for lonlat and node number
-		HashMap<LonLat, Integer> ll_num = new HashMap<LonLat, Integer>();
-		// result map of date and node number
-		HashMap<String, Integer> date_node = new HashMap<String, Integer>();
-		Integer clusnum = 1;
-		for(String date : date_ll.keySet()) {
-			LonLat p = date_ll.get(date);
-			// first point 
-			if(ll_num.isEmpty()) {
-				ll_num.put(p, clusnum);
-				date_node.put(date, clusnum);
-				clusnum+=1;
-			}
-			// from second point
-			else {
-				// check in all clusters
-				String flag = "no";
-				for(LonLat clus : ll_num.keySet()) {
-					if(p.distance(clus)<thres) {
-						date_node.put(date, ll_num.get(clus));
-						flag = "yes";
-						break;
-					}
-				}
-				// if point is not near any cluster center
-				if(flag.equals("no")) {
-					ll_num.put(p, clusnum);
-					date_node.put(date, clusnum);
-					clusnum+=1;
-				}
-			}
-		}
-		return date_node;
-	}
-	
-	
-	
-	
+
 }
